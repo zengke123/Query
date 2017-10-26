4ec288e711695d5d3191c455145ba5fad4a0e2ef
zengke123 <30573254+zengke123@users.noreply.github.com>
2017-10-26 22:43:24 +0800

    Update query.py

--- query.py
+++ query.py
@@ -3,7 +3,7 @@
 """命令行查看工具
 
 Usage:
-    tickets [-MgdtkzTncW]  ((<from>  <to>  <date>)|()| (<city>))
+    query [-MgdtkzTncW]  ((<from>  <to>  <date>)|()| (<city>))
 
 Options:
     -h,--help   显示帮助菜单
@@ -21,10 +21,10 @@ Options:
     -W          天气查询
 
 Example:
-    tickets -T 北京 上海 2017-10-24
-    tickets -T 成都 南京 2017-10-24 -dg
-    tickets -M
-    tickets -W 四川大学
+    query -T 北京 上海 2017-10-24
+    query -T 成都 南京 2017-10-24 -dg
+    query -M
+    query -W 四川大学
 """
 
 import train
